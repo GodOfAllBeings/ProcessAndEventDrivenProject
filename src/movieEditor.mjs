0b4f8b63d9caae6c2a4aea7774e21afdb3f70545
GodOfAllBeings <s174871@student.dtu.dk>
2022-03-18 13:33:22 +0100

    Commented some of the unused stuff

--- src/movieEditor.mjs
+++ src/movieEditor.mjs
@@ -1,28 +1,25 @@
-// const express = require('express');
-// const cors = require('cors');
-// const multer = require('multer');
 import express from 'express';
 import cors from 'cors';
-import multer from 'multer';
+// import multer from 'multer';
 import { createFFmpeg } from '@ffmpeg/ffmpeg';
 
 const app = express();
-const port = 3000;
+// const port = 3000;
 
-const upload = multer({
-    storage: multer.memoryStorage(),
-    limits: { fileSize: 100 * 1024 * 1024 }
-});
+// const upload = multer({
+//     storage: multer.memoryStorage(),
+//     limits: { fileSize: 100 * 1024 * 1024 }
+// });
 
-app.post('/thumbnail', upload.single('video'), async (req, res) => {
-    const videoData = req.file.buffer;
+// app.post('/thumbnail', upload.single('video'), async (req, res) => {
+//     const videoData = req.file.buffer;
 
-    res.sendStatus(200);
-});
+//     res.sendStatus(200);
+// });
 
-app.listen(port, () => {
-    console.log(`[info] ffmpeg-api listening at http://localhost:${port}`)
-});
+// app.listen(port, () => {
+//     console.log(`[info] ffmpeg-api listening at http://localhost:${port}`)
+// });
 
 
 const ffmpegInstance = createFFmpeg({ log: true });
