61504870c59c5d7f3e4ea9dd9f813aa78db9c388
Squidman95 <squidmanse@gmail.com>
2022-03-22 14:44:31 +0100

    Works!

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ var subDirectory = "public/uploads";
 
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
-
   fs.mkdirSync(subDirectory);
 }
 
@@ -53,21 +52,44 @@ app.get("/", (req, res) => {
 
 app.post("/merge", upload.array("files", 1000), (req, res) => {
   list = "";
+  let ffm = "ffmpeg";
+  let filters = ' -filter_complex "';
+  let filterEnd = "";
   if (req.files) {
+    for (let i = 0; i < req.files.length; i++) {
+      ffm += ` -i public/uploads/${req.files[i].filename}`;
+      filters += `[${i}]crop=720:720:280:0, scale=640:640, fps=30[${i}v];`;
+      filterEnd += `[${i}v]`;
+    }
+    filterEnd += `concat=n=${req.files.length}:v=1:a=0[v]" -map "[v]" ${outputFilePath}`;
+
+    ffm += filters;
+    ffm += filterEnd;
     req.files.forEach((file) => {
       list += `file ${file.filename}`;
       list += "\n";
     });
+
+    console.log(ffm);
 
     var writeStream = fs.createWriteStream(listFilePath);
 
     writeStream.write(list);
 
     writeStream.end();
-
     exec(
-      //   `ffmpeg -f concat -i public/uploads/1647942976921list.txt -c copy public/uploads/1647942976921output.mp4`,
-      `ffmpeg -f concat -i ${listFilePath} -c copy ${outputFilePath}`,
+      // Old way of doing it. No longer uses the .txt file.
+      // `ffmpeg -f concat -i ${listFilePath} -c copy ${outputFilePath}`,
+
+      // Crops, scales and sets fixed FPS. The String ffm should create something like this example:
+      // `ffmpeg -i assets/ad1.mp4 -i assets/cat1.mp4 -i assets/dog1.mp4 -filter_complex \
+      // "[0]crop=720:720:280:0, scale=640:640, fps=30[0v]; \
+      // [1]crop=720:720:280:0, scale=640:640, fps=30[1v]; \
+      // [2]crop=720:720:280:0, scale=640:640, fps=30[2v]; \
+      // [0v][1v][2v]concat=n=3:v=1:a=0[v]" \
+      // -map "[v]" ${outputFilePath}`,
+      ffm,
+
       (error, stdout, stderr) => {
         if (error) {
           console.log(`error: ${error.message}`);
@@ -76,11 +98,9 @@ app.post("/merge", upload.array("files", 1000), (req, res) => {
           console.log("videos are successfully merged");
           res.download(outputFilePath, (err) => {
             if (err) throw err;
-
             req.files.forEach((file) => {
               fs.unlinkSync(file.path);
             });
-
             fs.unlinkSync(listFilePath);
             fs.unlinkSync(outputFilePath);
           });
