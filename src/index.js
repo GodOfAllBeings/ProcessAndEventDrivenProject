6e4902ba11edbe54b172b0f8f7fcd9806b62af58
Uniel <adam@saunte.dk>
2022-04-01 15:08:08 +0200

    Added concatVideo function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
-var outputFilePath = Date.now() + "output.mp4";
+var outputFilePath = Date.now() + "_output.mp4()";
 const bodyParser = require("body-parser");
 const multer = require("multer");
 const app = express();
@@ -48,48 +48,77 @@ var subDirectory = "public/uploads";
 //   res.sendFile(__dirname + "/index.html");
 // });
 
+function updateOutputFilePath(trend) {
+  outputFilePath = Date.now() + (trend? "_" + trend : "" ) + "_output.mp4";
+}
+
+function assetPath(fileName) {
+  return path.join(__dirname, "../assets/videos", fileName);
+}
+
+function compilationPath(fileName) {
+  return path.join(__dirname, "../assets/compilations", fileName);
+}
+
+function merge(files, trend = "") {
+  // figure out if the files passed are strings
+  //let types = files.map(file => typeof file);
+  // If they are - read the files
+  let paths = files.map(file => assetPath(file));
+
+  if(trend) {updateOutputFilePath(trend);}
+
+  let ffm = "ffmpeg";
+  let filters = ' -filter_complex "';
+  let filterEnd = "";
+  if (paths) {
+    for (let i = 0; i < paths.length; i++) {
+      ffm += ` -i ${paths[i]}`;
+      filters += `[${i}]crop=720:720:280:0, scale=640:640, fps=30[${i}v];`;
+      filterEnd += `[${i}v]`;
+    }
+    filterEnd += `concat=n=${paths.length}:v=1:a=0[v]" -map "[v]" ${compilationPath(outputFilePath)}`;
+
+    ffm += filters;
+    ffm += filterEnd;
+    console.log(ffm);
+    exec(
+      // Crops, scales and sets fixed FPS. The String ffm should create something like this example:
+      // `ffmpeg -i assets/ad1.mp4 -i assets/cat1.mp4 -i assets/dog1.mp4 -filter_complex \
+      // "[0]crop=720:720:280:0, scale=640:640, fps=30[0v]; \
+      // [1]crop=720:720:280:0, scale=640:640, fps=30[1v]; \
+      // [2]crop=720:720:280:0, scale=640:640, fps=30[2v]; \
+      // [0v][1v][2v]concat=n=3:v=1:a=0[v]" \
+      // -map "[v]" ${outputFilePath}`,
+      ffm,
+
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        } else {
+          console.log("videos are successfully merged");
+          res.download(outputFilePath, (err) => {
+            if (err) throw err;
+          });
+        }
+      }
+    );
+  }
+  return compilationPath(outputFilePath);
+
+}
+
 // app.post("/merge", upload.array("files", 1000), (req, res) => {
-//   let ffm = "ffmpeg";
-//   let filters = ' -filter_complex "';
-//   let filterEnd = "";
-//   if (req.files) {
-//     for (let i = 0; i < req.files.length; i++) {
-//       ffm += ` -i public/uploads/${req.files[i].filename}`;
-//       filters += `[${i}]crop=720:720:280:0, scale=640:640, fps=30[${i}v];`;
-//       filterEnd += `[${i}v]`;
-//     }
-//     filterEnd += `concat=n=${req.files.length}:v=1:a=0[v]" -map "[v]" ${outputFilePath}`;
+//   let outputFilePath = merge(req.files);
 
-//     ffm += filters;
-//     ffm += filterEnd;
-//     console.log(ffm);
-//     exec(
-//       // Crops, scales and sets fixed FPS. The String ffm should create something like this example:
-//       // `ffmpeg -i assets/ad1.mp4 -i assets/cat1.mp4 -i assets/dog1.mp4 -filter_complex \
-//       // "[0]crop=720:720:280:0, scale=640:640, fps=30[0v]; \
-//       // [1]crop=720:720:280:0, scale=640:640, fps=30[1v]; \
-//       // [2]crop=720:720:280:0, scale=640:640, fps=30[2v]; \
-//       // [0v][1v][2v]concat=n=3:v=1:a=0[v]" \
-//       // -map "[v]" ${outputFilePath}`,
-//       ffm,
-
-//       (error, stdout, stderr) => {
-//         if (error) {
-//           console.log(`error: ${error.message}`);
-//           return;
-//         } else {
-//           console.log("videos are successfully merged");
-//           res.download(outputFilePath, (err) => {
-//             if (err) throw err;
-//             req.files.forEach((file) => {
-//               fs.unlinkSync(file.path);
-//             });
-//             // fs.unlinkSync(outputFilePath);
-//           });
-//         }
-//       }
-//     );
-//   }
+//   res.download(outputFilePath, (err) => {
+//     if (err) throw err;
+//     req.files.forEach((file) => {
+//       fs.unlinkSync(file.path);
+//     });
+//     // fs.unlinkSync(outputFilePath);
+//   });
 // });
 
 // http://127.0.0.1:3000/
@@ -119,9 +148,14 @@ const doesVideosForTrendExists = (trend) => {
 const schedule = require('node-schedule');
 const Trends = require('./trends_test.js');
 const TrendFilter = require('./trendFilter.js');
+const { exit } = require("process");
+
+// concatVideo("cat");
+// exit();
 
 // Scheduler is * * * * * * --> Second(0-60) Minute(0-60) Hour(0-23) dayOfMonth(1-31) Month(1-12) dayOfWeek(1-7)
-var scheduler = schedule.scheduleJob('0 0 20 * * *', function(){
+var scheduler = schedule.scheduleJob('0 9 * * * *', function(){
+
   const countries = ["CA"];//, "DE", "NO", "US", "SE"]; // "GB", "FR", "AU", "BE", "CH"
   console.log(new Date());
   
@@ -135,3 +169,10 @@ var scheduler = schedule.scheduleJob('0 0 20 * * *', function(){
   // trend = findBestTrend();
   // doesVideosForTrendExists(trend) ? concatVideo(trend) : null
 });
+
+// We know there are at least a couple (minimalVideos) amount of videos.
+// Lets find them and create a compilation
+function concatVideo(trend) {
+  let videosMatchingTrend = getAllVideoWithPartOfName(trend);
+  let outputFilePath = merge(videosMatchingTrend, trend);
+}
