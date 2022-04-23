d850c66b29ed9b5e8932e10d3b6414ede336e764
Jonathan S. Winther <jonathanswinther@gmail.com>
2022-04-23 12:51:41 +0200

    create merchandise completes flow with postman

--- src/scripts/view_generator.py
+++ src/scripts/view_generator.py
@@ -11,9 +11,9 @@ url = 'http://localhost:8081/ViewOnVideo'
 # url = 'http://localhost:3000/'
 
 while True:
-    video = random.choice(dict)
-    print(video)
-    fields={'value': video}
+    topic = random.choice(dict)
+    print(topic)
+    fields={'topic': topic}
     encoded = json.dumps(fields).encode('utf-8')
     r = http.request('POST', url, body=encoded, headers={'Content-Type': 'application/json'})
     time.sleep(random.random())
