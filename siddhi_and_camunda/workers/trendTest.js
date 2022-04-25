8d328c6e9853a0e04925317b5141916bcaa63063
Squidman95 <squidmanse@gmail.com>
2022-04-25 08:49:59 +0200

    Update

--- siddhi_and_camunda/workers/trendTest.js
+++ siddhi_and_camunda/workers/trendTest.js
@@ -32,4 +32,29 @@ client.subscribe("TrendMood", async function({ task, taskService }) {
 
   // First version of complete the task
   await taskService.complete(task, processVariables);
+
+  taskService.complete
 });
+
+client.subscribe("SendTrend", async function({ task, taskService }) {
+    // Put your business logic
+    const bookTitle = "Sup ya'll, it's ya boi Asmongold";
+    console.log("** Reminder to read: " + bookTitle + "**");
+  
+    const austriaResponse = "Sorry Laff can't help. BTW, Prussia wants a quick word with you...";
+    const processVariables = new Variables();
+    processVariables.set("austriaResponse", austriaResponse);
+  
+  
+  //   // Second version
+  //   if(bookTitle.includes("Austria")) {
+  //     // complete the task
+  //     await taskService.complete(task, processVariables);
+  //   } else {
+  //     // throw a BPMN error
+  //     await taskService.handleBpmnError(task, "REFUSE_HELP", "Sorry! We're super busy, you are")
+  //   }
+  
+    // First version of complete the task
+    await taskService.complete(task, processVariables);
+  });
