5910afff22252ea145e33bdc7af7cd55261ef65b
Uniel <adam@saunte.dk>
2022-04-26 10:11:51 +0200

    Fixed decision table in CreateMerch

--- src/scripts/view_generator.py
+++ src/scripts/view_generator.py
@@ -10,6 +10,19 @@ http = urllib3.PoolManager()
 url = 'http://localhost:8081/ViewOnVideo'
 # url = 'http://localhost:3000/'
 
+debugging = True
+if debugging:
+    i = 0
+    while i < 20: 
+        video = "cat"
+        print(video)
+        fields={'video': video}
+        encoded = json.dumps(fields).encode('utf-8')
+        r = http.request('POST', url, body=encoded, headers={'Content-Type': 'application/json'})
+        time.sleep(random.random()/2)
+        i = i + 1
+    exit()
+
 while True:
     video = random.choice(dict)
     print(video)
