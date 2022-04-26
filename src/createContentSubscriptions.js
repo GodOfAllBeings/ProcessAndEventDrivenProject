1a70687c508d8a36ea27e4e8db8a0943c8eda947
GodOfAllBeings <s174871@student.dtu.dk>
2022-04-26 09:37:34 +0200

    createContent model work in camunda

---
+++ src/createContentSubscriptions.js
@@ -0,0 +1,13 @@
+const { Client, logger } = require("camunda-external-task-client-js");
+const Variables = require("camunda-external-task-client-js/lib/Variables");
+
+const index = require('./index.js');
+
+const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
+const client = new Client(config);
+
+client.subscribe("CombineVideos", async function({ task, taskService }) {
+    // const videoLink = index.concatVideo("cat");
+    // console.log(videoLink);
+    await taskService.complete(task);
+});
